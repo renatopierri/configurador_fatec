359ea196a96ba211f1f676ae5b1daad63310be6c
Renato de Pierri <renatopierri@users.noreply.github.com>
2023-10-03 19:16:47 -0300

    Criacao do instalador 7zip
    Criacao do instalador 7zip

---
+++ aplicativos/instala_7zip.ps1
@@ -0,0 +1,18 @@
+﻿$instalador=$args[0]
+$diretorioCorrente = Get-Location
+
+Write-Host "Instalando 7zip."
+Invoke-Expression "$diretorioCorrente\aplicativos\$instalador"
+
+Write-host "Verificando se precisa adicionar 7zip na variavel %PATH%."
+if(!$env:Path.Contains("7-Zip")){
+    $novoPath = "$env:ProgramFiles\7-Zip;" + $env:Path
+    [Environment]::SetEnvironmentVariable("Path", "$novoPath", "Machine")
+    #Atualizando a variável de ambiente da sessão do Powershell
+    $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") # + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")  
+    write-host "`nVariavel %PATH% atualizada para:`n$env:Path`n"        
+}else{
+    write-host "Variavel %PATH% ja esta atualizada"
+}
+
+Write-Host "Instalado.`n"
