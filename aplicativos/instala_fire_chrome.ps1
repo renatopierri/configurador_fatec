9cff0421504c4896fc0f67a11f9b25e95e22b8cc
Renato de Pierri <renatopierri@users.noreply.github.com>
2023-10-05 14:54:27 -0300

    Criado instalador do Firefox e Chrome
    Criação do instalador do Firefox e Chrome

---
+++ aplicativos/instala_fire_chrome.ps1
@@ -0,0 +1,9 @@
+﻿$caminhho = "$env:ProgramFiles\Mozilla Firefox\uninstall"
+if (Test-Path $caminhho){
+    $desinstalador = "$caminhho\helper.exe"
+    &$desinstalador /S
+}
+choco install firefox --force --silent --y
+
+choco uninstall googlechrome -n
+choco install googlechrome --force --silent --y
